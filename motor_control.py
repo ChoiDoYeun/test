fcd2cd154930bb2005ccfd9f2f2feaff75f043f2
Choi Do Yeun <87837828+ChoiDoYeun@users.noreply.github.com>
2024-01-11 22:30:32 +0900

    Update motor_control.py

--- motor_control.py
+++ motor_control.py
@@ -36,7 +36,11 @@ def move_motor(step_pin, dir_pin, steps, direction):
 try:
     # 목표 좌표로 이동 ex : (X:100mm, Y:200mm, Z:100mm)
     move_motor(X_STEP, X_DIR, 100 * STEPS_PER_MM, GPIO.HIGH)  # X축 이동
+    time.sleep(100 * STEPS_PER_MM * 0.002 + 100)  # Y축 이동에 필요한 시간
+    
     move_motor(Y_STEP, Y_DIR, 200 * STEPS_PER_MM, GPIO.HIGH)  # Y축 이동
+    time.sleep(200 * STEPS_PER_MM * 0.002 + 100)  # Y축 이동에 필요한 시간
+    
     move_motor(Z_STEP, Z_DIR, 100 * STEPS_PER_MM, GPIO.HIGH)  # Z축 이동
 finally:
     GPIO.cleanup()
