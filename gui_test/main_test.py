5fc46156fbdbaa840ce4497d492c131cd5bf8aad
Choi Do Yeun <87837828+ChoiDoYeun@users.noreply.github.com>
2024-01-12 23:06:53 +0900

    Update main_test.py

--- gui_test/main_test.py
+++ gui_test/main_test.py
@@ -1,13 +1,13 @@
 # main_test.py
 import camera_gui
 import gui
-import tkinter as tk
 
 def main():
-    warehouse_data = camera_gui.process_data()  # camera.py에서 데이터 처리
-    root = tk.Tk()
-    root.title("Warehouse Management System")
-    gui.create_and_run_gui(root, warehouse_data)  # GUI 생성 및 실행
+    # Get the updated warehouse data from camera_gui
+    warehouse_data = camera_gui.process_data()
+    
+    # Create and run the GUI with the warehouse data
+    gui.create_gui(warehouse_data)
 
 if __name__ == "__main__":
     main()
