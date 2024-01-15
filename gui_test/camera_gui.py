6538402364ab38b468547daa713566611dec2988
Choi Do Yeun <87837828+ChoiDoYeun@users.noreply.github.com>
2024-01-15 13:45:36 +0900

    Update camera_gui.py

--- gui_test/camera_gui.py
+++ gui_test/camera_gui.py
@@ -3,7 +3,7 @@ from PIL import Image
 from pyzbar.pyzbar import decode
 
 def capture_image(image_path='captured_image.jpg'):
-    subprocess.run(['libcamera-still', '-o', image_path])
+    subprocess.run(['libcamera-still', '-n', '-o', image_path])
 
 def read_qr_code(image_path):
     img = Image.open(image_path)
