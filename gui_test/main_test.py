fde1e7c8da4faabcb40a76cc88e11f163efb10dd
Choi Do Yeun <87837828+ChoiDoYeun@users.noreply.github.com>
2024-01-15 13:41:01 +0900

    Update main_test.py

--- gui_test/main_test.py
+++ gui_test/main_test.py
@@ -1,28 +1,7 @@
-import time
 import threading
 from warehouse import Warehouse
-from camera_gui import capture_image, read_qr_code
 from gui import create_gui
-
-def process_qr_code(warehouse):
-    qr_code_image_path = 'captured_image.jpg'
-    while True:
-        capture_image(qr_code_image_path)
-        qr_data = read_qr_code(qr_code_image_path)
-
-        if qr_data:
-            cars = qr_data.split('\n')
-            for car in cars:
-                if car:
-                    location = warehouse.store_item(car)
-                    if location:
-                        print(f"{car}가 {location}에 저장되었습니다.")
-                    else:
-                        print(f"{car}를 저장할 공간이 없습니다.")
-        else:
-            print("QR 코드를 읽을 수 없습니다.")
-
-        time.sleep(2)  # Adjust the delay as needed
+from qr_processor import process_qr_code
 
 def main():
     warehouse = Warehouse()
@@ -30,10 +9,10 @@ def main():
     warehouse.load_existing_items(existing_items)
 
     qr_thread = threading.Thread(target=process_qr_code, args=(warehouse,))
-    qr_thread.daemon = True  # Daemon thread로 설정하여 메인 스레드 종료 시 함께 종료되도록 설정
+    qr_thread.daemon = True
     qr_thread.start()
 
-    create_gui(warehouse)  # 메인 스레드는 GUI 처리에 집중
+    create_gui(warehouse)
 
 if __name__ == "__main__":
     main()
