44936a8830cf5a8e6807e305a925c444943ecdcf
Choi Do Yeun <87837828+ChoiDoYeun@users.noreply.github.com>
2024-01-15 19:39:11 +0900

    Update main_test.py

--- gui_test/main_test.py
+++ gui_test/main_test.py
@@ -14,14 +14,16 @@ def main():
     
     # # 미리 저장된 차량 정보 로딩
     warehouse.load_existing_items(existing_items)
+
+    # gui 생성
+    create_gui(warehouse)
     
     # 멀티 쓰레딩
     qr_thread = threading.Thread(target=process_qr_code, args=(warehouse,))
     qr_thread.daemon = True
     qr_thread.start()
     
-    # gui 생성
-    create_gui(warehouse)
+
 
 if __name__ == "__main__":
     main()
