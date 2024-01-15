f118137212db5f41bb3afe5dff192e7c61fba061
Choi Do Yeun <87837828+ChoiDoYeun@users.noreply.github.com>
2024-01-15 13:51:14 +0900

    Update main_test.py
    주석 추가

--- gui_test/main_test.py
+++ gui_test/main_test.py
@@ -3,15 +3,23 @@ from warehouse import Warehouse
 from gui import create_gui
 from qr_processor import process_qr_code
 
+# main
 def main():
+    # 객체 생성
     warehouse = Warehouse()
+    
+    # 미리 저장된 차량 정보
     existing_items = {'A12': '차량1', 'A21': '차량2', 'A31': '차량3'}
+    
+    # # 미리 저장된 차량 정보 로딩
     warehouse.load_existing_items(existing_items)
-
+    
+    # 멀티 쓰레딩
     qr_thread = threading.Thread(target=process_qr_code, args=(warehouse,))
     qr_thread.daemon = True
     qr_thread.start()
-
+    
+    # gui 생성
     create_gui(warehouse)
 
 if __name__ == "__main__":
