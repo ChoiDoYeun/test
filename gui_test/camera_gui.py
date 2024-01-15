e8d61c51299b2519309772f0b185939b89a5a935
Choi Do Yeun <87837828+ChoiDoYeun@users.noreply.github.com>
2024-01-15 13:47:53 +0900

    Update camera_gui.py
    주석추가

--- gui_test/camera_gui.py
+++ gui_test/camera_gui.py
@@ -2,9 +2,11 @@ import subprocess
 from PIL import Image
 from pyzbar.pyzbar import decode
 
+# 카메라로 캡처
 def capture_image(image_path='captured_image.jpg'):
     subprocess.run(['libcamera-still', '-n', '-o', image_path])
 
+# qr코드 리더
 def read_qr_code(image_path):
     img = Image.open(image_path)
     decoded_objects = decode(img)
