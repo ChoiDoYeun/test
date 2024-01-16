d7da4987a3e3f86c5f92c8a4d2a60ee52d07c91c
Choi Do Yeun <87837828+ChoiDoYeun@users.noreply.github.com>
2024-01-16 12:32:56 +0900

    Update main_test.py

--- gui_test/main_test.py
+++ gui_test/main_test.py
@@ -9,22 +9,18 @@ def main():
     print("start")
     # 객체 생성
     warehouse = Warehouse()
-    
+
     # 미리 저장된 차량 정보
     existing_items = {'A12': '차량1', 'A21': '차량2', 'A31': '차량3'}
-    
-    # # 미리 저장된 차량 정보 로딩
+
+    # 미리 저장된 차량 정보 로딩
     warehouse.load_existing_items(existing_items)
 
     # gui 생성
     create_gui(warehouse)
-    
-    # 멀티 쓰레딩
-    qr_thread = threading.Thread(target=process_qr_code, args=(warehouse,))
-    qr_thread.daemon = True
-    qr_thread.start()
-    
 
+    # QR 코드 처리 함수 직접 호출
+    process_qr_code(warehouse)
 
 if __name__ == "__main__":
     main()
