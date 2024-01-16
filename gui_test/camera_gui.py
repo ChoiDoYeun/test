c36f577e9a1e26fe15a4af178e548a3e709091fa
Choi Do Yeun <87837828+ChoiDoYeun@users.noreply.github.com>
2024-01-16 12:22:05 +0900

    Update camera_gui.py

--- gui_test/camera_gui.py
+++ gui_test/camera_gui.py
@@ -6,6 +6,7 @@ from pyzbar.pyzbar import decode
 # 카메라로 캡처
 def capture_image(image_path='captured_image.jpg'):
     subprocess.run(['libcamera-still', '-n', '-o', image_path])
+    print("camera ready")
 
 # qr코드 리더
 def read_qr_code(image_path):
