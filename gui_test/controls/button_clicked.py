559fb955d1449236f6c8ce145fdcc808c853b37e
Choi Do Yeun <87837828+ChoiDoYeun@users.noreply.github.com>
2024-01-19 15:08:37 +0900

    Update button_clicked.py

--- gui_test/controls/button_clicked.py
+++ gui_test/controls/button_clicked.py
@@ -1,2 +1,15 @@
+from motors.move_motor import move_position
+
+# 전역 변수로 Warehouse 인스턴스를 관리하거나, 다른 메커니즘을 사용해야 합니다.
+warehouse = None
+
+def set_warehouse_instance(warehouse_instance):
+    global warehouse
+    warehouse = warehouse_instance
+
 def button_clicked(button_text):
-    print("Selected area:", button_text)
+    if warehouse is not None:
+        x, y, z = warehouse.calculate_coordinates(button_text)
+        move_position(x, y, z)
+    else:
+        print("Warehouse instance is not set.")
