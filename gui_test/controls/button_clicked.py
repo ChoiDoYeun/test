d03146e3b625c01872b3454fc8dfa65a9be2b257
Choi Do Yeun <87837828+ChoiDoYeun@users.noreply.github.com>
2024-01-19 15:21:23 +0900

    Update button_clicked.py

--- gui_test/controls/button_clicked.py
+++ gui_test/controls/button_clicked.py
@@ -1,4 +1,5 @@
 from move_position import move_position
+from motor.calculate_steps_and_direction import calculate_steps_and_direction
 
 # 전역 변수로 Warehouse 인스턴스를 관리하거나, 다른 메커니즘을 사용해야 합니다.
 warehouse = None
