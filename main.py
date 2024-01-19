8eefa5c88d7edd43faaa7333d586b72c26e855c4
Choi Do Yeun <87837828+ChoiDoYeun@users.noreply.github.com>
2024-01-19 18:47:23 +0900

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-# main.py
-import camera  # camera.py 모듈을 임포트합니다.
-
-def main():
-    # camera.py의 main_loop 함수를 호출하여 카메라 처리를 시작합니다.
-    camera.main_loop()
-
-if __name__ == "__main__":
-    main()
