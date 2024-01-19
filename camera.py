f380c255767757bd4422b1d5867afa7c9dc9a490
Choi Do Yeun <87837828+ChoiDoYeun@users.noreply.github.com>
2024-01-19 18:58:16 +0900

    Add files via upload

---
+++ camera.py
@@ -0,0 +1,15 @@
+# camera.py
+import subprocess
+from PIL import Image
+from pyzbar.pyzbar import decode
+
+# 카메라로 캡처
+def capture_image(image_path='captured_image.jpg'):
+    subprocess.run(['libcamera-still','-n', '-o', image_path])
+    print("camera ready")
+
+# qr코드 리더
+def read_qr_code(image_path):
+    img = Image.open(image_path)
+    decoded_objects = decode(img)
+    return decoded_objects[0].data.decode() if decoded_objects else ""
---
+++ main.py
@@ -0,0 +1,23 @@
+import threading
+from qr_processor import process_qr_code
+from gui import create_gui
+from warehouse_class import Warehouse
+from location_selected_callback import location_selected_callback
+
+def main():
+    warehouse = Warehouse()
+    # 미리 저장된 차량 정보
+    existing_items = {'A12': '차량1', 'A21': '차량2', 'A31': '차량3'}
+
+    # # 미리 저장된 차량 정보 로딩
+    warehouse.load_existing_items(existing_items)
+    
+    # 멀티 쓰레딩
+    qr_thread = threading.Thread(target=process_qr_code, args=(warehouse,))
+    qr_thread.daemon = True
+    qr_thread.start()
+    
+    create_gui(warehouse, callback=location_selected_callback)
+    
+if __name__ == "__main__":
+    main()
