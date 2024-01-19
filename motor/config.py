96a07dc15bea2f5b207d03f928781a40f83ed53d
Choi Do Yeun <87837828+ChoiDoYeun@users.noreply.github.com>
2024-01-19 19:00:54 +0900

    Add files via upload

---
+++ motor/config.py
@@ -0,0 +1,17 @@
+# config.py
+STEPS_PER_MM = 200
+
+# Global position variables
+current_x = 0
+current_y = 0
+current_z = 0
+
+def initialize():
+    global current_x, current_y, current_z
+    X_STEP = 17
+    X_DIR = 18
+    Y_STEP = 27
+    Y_DIR = 22
+    Z_STEP = 23
+    Z_DIR = 24
+    return X_STEP, X_DIR, Y_STEP, Y_DIR, Z_STEP, Z_DIR
