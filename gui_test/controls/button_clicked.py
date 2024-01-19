ae0c5ee75cd835e6312db1ee723ebd4c28c725fd
Choi Do Yeun <87837828+ChoiDoYeun@users.noreply.github.com>
2024-01-19 15:37:22 +0900

    Update button_clicked.py

--- gui_test/controls/button_clicked.py
+++ gui_test/controls/button_clicked.py
@@ -4,14 +4,17 @@ from warehouse import Warehouse
 
 # 전역 변수로 Warehouse 인스턴스를 관리하거나, 다른 메커니즘을 사용해야 합니다.
 warehouse = None
+global_coordinates = {'x': 0, 'y': 0, 'z': 0}
+
 
 def set_warehouse_instance(warehouse_instance):
-    global warehouse
+    global warehouse,global_coordinates
     warehouse = warehouse_instance
 
 def button_clicked(button_text):
     if warehouse is not None:
         x, y, z = warehouse.calculate_coordinates(button_text)
+        global_coordinates.update({'x': x, 'y': y, 'z': z})
         move_position(x, y, z)
     else:
         print("Warehouse instance is not set.")
