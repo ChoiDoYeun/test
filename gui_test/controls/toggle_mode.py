28fa86cafab9859db9d45bdc2daa0e87eabe1f1a
Choi Do Yeun <87837828+ChoiDoYeun@users.noreply.github.com>
2024-01-19 14:27:09 +0900

    Update toggle_mode.py

--- gui_test/controls/toggle_mode.py
+++ gui_test/controls/toggle_mode.py
@@ -1,5 +1,5 @@
-    def toggle_mode(button, mode, mode_name):
-        mode['state'] = not mode['state']
-        new_state = "ON" if mode['state'] else "OFF"
-        button.config(bg='green' if mode['state'] else 'SystemButtonFace')
-        print(f"{mode_name} mode is now {new_state}")
+def toggle_mode(button, mode, mode_name):
+    mode['state'] = not mode['state']
+    new_state = "ON" if mode['state'] else "OFF"
+    button.config(bg='green' if mode['state'] else 'SystemButtonFace')
+    print(f"{mode_name} mode is now {new_state}")
