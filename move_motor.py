5c060c523a29a681dc46b5f48202dd31db9576d9
Choi Do Yeun <87837828+ChoiDoYeun@users.noreply.github.com>
2024-02-01 11:55:34 +0900

    Update move_motor.py

--- move_motor.py
+++ move_motor.py
@@ -1,3 +1,12 @@
+# 조건
+# steps/rev = 400
+# microsteps = 2
+# screw pitch = 2mm
+
+# step/mm = (steps/rev * microsteps) / screw pitch
+#         = (400 * 2) / 2
+#         = 400 step/mm
+# 스텝수 제어
 import RPi.GPIO as GPIO
 import time
 
@@ -10,83 +19,28 @@ Z_STEP = 23  # Z축 STEP 핀 번호
 Z_DIR = 24   # Z축 DIR 핀 번호
 
 # 스텝 설정
-STEPS_PER_MM = 200  # 1mm당 필요한 스텝 수
+STEPS_PER_MM = 400  # 1mm당 필요한 스텝 수
 
 # GPIO 설정
 GPIO.setmode(GPIO.BCM)
 GPIO.setup([X_STEP, X_DIR, Y_STEP, Y_DIR, Z_STEP, Z_DIR], GPIO.OUT)
 
-# 현재위치 초기화
-current_x = 0
-current_y = 0
-current_z = 0
-
-# 모터 동작 함수
 def move_motor(step_pin, dir_pin, steps, direction):
     GPIO.output(dir_pin, direction)
     for _ in range(steps):
         GPIO.output(step_pin, GPIO.HIGH)
-        time.sleep(0.00025)  # 1ms 대기
+        time.sleep(0.001)  # 1ms 대기
         GPIO.output(step_pin, GPIO.LOW)
-        time.sleep(0.00025)
-      
-# dir, step 계산 함수
-def calculate_steps_and_direction(current_pos, target_pos):
-    if target_pos > current_pos:
-        direction = GPIO.HIGH
-        steps = (target_pos - current_pos) * STEPS_PER_MM
-    else:
-        direction = GPIO.LOW
-        steps = (current_pos - target_pos) * STEPS_PER_MM
-    return steps, direction
+        time.sleep(0.001)
 
 try:
-    while True:
-        # 사용자로부터 X, Y, Z축의 목표 위치 입력 받기 
-        # 이후 메인에서 qr코드의 위치값으로 대체
-        # target_x, target_y, target_z = warehouse.calculate_coordinates(location)으로 변경? -> import warehouse 해야함
-        target_x = int(input("target X : "))
-        target_y = int(input("target Y : "))
-        target_z = int(input("target Z : "))
-
-        # X축 이동
-        steps, direction = calculate_steps_and_direction(current_x, target_x) # X축 dir방향, step수 계산
-        move_motor(X_STEP, X_DIR, steps, direction) # X축 모터 동작
-        time.sleep(steps * 0.002)  # 대기 시간
-        current_x = target_x  # 현재 X 위치 업데이트
-
-        # Y축 이동
-        steps, direction = calculate_steps_and_direction(current_y, target_y) # Y축 dir방향, step수 계산
-        move_motor(Y_STEP, Y_DIR, steps, direction) # Y축 모터 동작
-        time.sleep(steps * 0.002)  # 대기 시간
-        current_y = target_y  # 현재 Y 위치 업데이트
-
-        # Z축 이동
-        steps, direction = calculate_steps_and_direction(current_z, target_z) # Z축 dir방향, step수 계산
-        move_motor(Z_STEP, Z_DIR, steps, direction) # Z축 모터 동작
-        current_z = target_z  # 현재 Z 위치 업데이트
-
-        # 현재위치 출력
-        print("finish move to target position")
-        print("current position: X = {}, Y = {}, Z = {}".format(current_x, current_y, current_z))
-        
-        # 다음 명령을 받기 전에 짧은 시간 대기
-        time.sleep(0.5)
-
-except KeyboardInterrupt: #추후 stop버튼 푸쉬시 동작하도록 변경해야함
-    print("Before finish, should move to original. plz wait")
-    # 현재 위치에서 (0,0,0)으로 이동
-    steps, direction = calculate_steps_and_direction(current_x, 0)
-    move_motor(X_STEP, X_DIR, steps, direction)
-    time.sleep(steps * 0.002)
-
-    steps, direction = calculate_steps_and_direction(current_y, 0)
-    move_motor(Y_STEP, Y_DIR, steps, direction)
-    time.sleep(steps * 0.002)
-
-    steps, direction = calculate_steps_and_direction(current_z, 0)
-    move_motor(Z_STEP, Z_DIR, steps, direction)
-    time.sleep(steps * 0.002)
-    print("Arrived original, program finish")
+    # 목표 좌표로 이동 ex : (X:100mm, Y:200mm, Z:100mm)
+    move_motor(X_STEP, X_DIR, 100 * STEPS_PER_MM, GPIO.HIGH)  # X축 이동
+    time.sleep(100 * STEPS_PER_MM * 0.002 + 100)  # Y축 이동에 필요한 시간
+    
+    move_motor(Y_STEP, Y_DIR, 200 * STEPS_PER_MM, GPIO.HIGH)  # Y축 이동
+    time.sleep(200 * STEPS_PER_MM * 0.002 + 100)  # Y축 이동에 필요한 시간
+    
+    move_motor(Z_STEP, Z_DIR, 100 * STEPS_PER_MM, GPIO.HIGH)  # Z축 이동
 finally:
     GPIO.cleanup()
