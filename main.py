e8623645e9aef63912f05f755a9a7d596eb51eb7
ChoiDoYeun <fff0803@naver.com>
2024-03-03 19:25:53 +0900

    임포트 오류수정 시도

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import threading
-from qr_processor import process_qr_code
+# main.py
 from gui import create_gui
 from warehouse_class import Warehouse
 from location_selected_callback import location_selected_callback
+
 
 def main():
     warehouse = Warehouse()
@@ -11,11 +11,6 @@ def main():
 
     # # 미리 저장된 차량 정보 로딩
     warehouse.load_existing_items(existing_items)
-    
-    # 멀티 쓰레딩
-    qr_thread = threading.Thread(target=process_qr_code, args=(warehouse,))
-    qr_thread.daemon = True
-    qr_thread.start()
     
     create_gui(warehouse, callback=location_selected_callback)
     
