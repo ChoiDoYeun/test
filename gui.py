688e0579ccbdece37c99fda1dbcb2ef739f98c1f
Choi Do Yeun <87837828+ChoiDoYeun@users.noreply.github.com>
2024-05-13 15:49:13 +0900

    해상도 맞춤 수정

--- gui.py
+++ gui.py
@@ -9,14 +9,14 @@ from controls.update_labels import update_warehouse_labels
 def create_gui(warehouse, callback=None):
     root = tk.Tk()
     root.title("Warehouse Management System")
-    root.geometry("800x480")  # 해상도
+    root.geometry("750x450")  # 해상도
 
     # 창고 위치 레이블 관리를 위한 딕셔너리
     warehouse_labels = {}
 
     # 현재 창고 위치 섹션
     warehouse_frame = tk.LabelFrame(root, text="CURRENT WAREHOUSE POSITION")
-    warehouse_frame.grid(row=0, column=0, padx=10, pady=10)
+    warehouse_frame.grid(row=0, column=0, padx=5, pady=5)
 
     for i, building in enumerate(['A', 'B', 'C', 'D']):
         frame = tk.Frame(warehouse_frame, bd=2, relief='sunken')
@@ -25,12 +25,12 @@ def create_gui(warehouse, callback=None):
             for room in range(1, 3):
                 location = f'{building}{floor}{room}'
                 label_text = warehouse.storage[location] if warehouse.storage[location] else ' '
-                label = tk.Label(frame, text=label_text, borderwidth=1, relief="solid", width=10, height=5)
+                label = tk.Label(frame, text=label_text, borderwidth=1, relief="solid", width=8, height=3)
                 label.grid(row=3-floor, column=room-1, padx=5, pady=5)
                 # 레이블을 딕셔너리에 저장
                 warehouse_labels[location] = label
 
-        label = tk.Label(frame, text=building, font=("Arial", 16), width=5, height=2)
+        label = tk.Label(frame, text=building, font=("Arial", 12), width=2, height=2)
         label.grid(row=3, column=0, columnspan=2, pady=5)
 
     # 업데이트 함수 호출
@@ -57,25 +57,25 @@ def create_gui(warehouse, callback=None):
 
     # 모드 선택 섹션
     mode_frame = tk.Frame(root)
-    mode_frame.grid(row=1, column=0, padx=10, pady=10, sticky='w')
+    mode_frame.grid(row=1, column=0, padx=2, pady=2, sticky='w')
 
-    input_on_button = tk.Button(mode_frame, text="INPUT ON", width=10, height=5, command=lambda: input_on(warehouse))
-    input_on_button.grid(row=0, column=0, padx=5, pady=5)  # grid를 사용하여 버튼 위치 지정
+    input_on_button = tk.Button(mode_frame, text="INPUT ON", width=8, height=3, command=lambda: input_on(warehouse))
+    input_on_button.grid(row=0, column=0, padx=2, pady=2)  # grid를 사용하여 버튼 위치 지정
 
-    input_off_button = tk.Button(mode_frame, text="INPUT OFF", width=10, height=5, command=input_off)
-    input_off_button.grid(row=0, column=1, padx=5, pady=5)  # 나란히 배치
+    input_off_button = tk.Button(mode_frame, text="INPUT OFF", width=8, height=3, command=input_off)
+    input_off_button.grid(row=0, column=1, padx=2, pady=2)  # 나란히 배치
 
-    output_on_button = tk.Button(mode_frame, text="OUTPUT ON", width=10, height=5, command=lambda: output_on(warehouse))
-    output_on_button.grid(row=0, column=2, padx=5, pady=5)
+    output_on_button = tk.Button(mode_frame, text="OUTPUT ON", width=8, height=3, command=lambda: output_on(warehouse))
+    output_on_button.grid(row=0, column=2, padx=2, pady=2)
 
-    output_off_button = tk.Button(mode_frame, text="OUTPUT OFF", width=10, height=5, command=output_off)
-    output_off_button.grid(row=0, column=3, padx=5, pady=5)
+    output_off_button = tk.Button(mode_frame, text="OUTPUT OFF", width=8, height=3, command=output_off)
+    output_off_button.grid(row=0, column=3, padx=2, pady=2)
 
     # 실행 및 중지 버튼 섹션
     control_frame = tk.Frame(root)
     control_frame.grid(row=1, column=0, padx=10, pady=10, sticky='e')
 
-    tk.Button(control_frame, text="RUN", bg="green", command=lambda: run(warehouse), width=10, height=5).pack(side=tk.LEFT, padx=5, pady=5)
-    tk.Button(control_frame, text="STOP", bg="red", command=stop, width=10, height=5).pack(side=tk.RIGHT, padx=5, pady=5)
+    tk.Button(control_frame, text="RUN", bg="green", command=lambda: run(warehouse), width=8, height=3).pack(side=tk.LEFT, padx=5, pady=5)
+    tk.Button(control_frame, text="STOP", bg="red", command=stop, width=8, height=3).pack(side=tk.RIGHT, padx=5, pady=5)
 
     root.mainloop()
