899d91464ff80be7edfd3a5a27a07a4c89ae9f4d
Choi Do Yeun <87837828+ChoiDoYeun@users.noreply.github.com>
2024-05-19 11:31:09 +0900

    모듈화 수정 capure_image

--- camera.py
+++ camera.py
@@ -1,13 +1,9 @@
-# camera.py
 import subprocess
+import torch
+import cv2
+import numpy as np
 from PIL import Image
 from pyzbar.pyzbar import decode
-
-# 카메라로 캡처
-#def capture_image(image_path='captured_image.jpg'):
-#    subprocess.run(['libcamera-still','-n', '-o', image_path])
-#    print("camera ready")
-
 
 # 감지된 객체 크롭하고 저장하는 함수
 def crop_object(results, img_pil, output_path):
@@ -27,7 +23,6 @@ def read_qr_code(image_path):
     img = Image.open(image_path)
     decoded_objects = decode(img)
     return decoded_objects[0].data.decode() if decoded_objects else ""
-
 
 def capture_image(output_path):
     # 경로 설정
