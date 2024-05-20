bab8e8c52f19a2692d523b952ccd44c869858082
Choi Do Yeun <87837828+ChoiDoYeun@users.noreply.github.com>
2024-05-20 15:04:12 +0900

    move_motor 파라미터값 수정

--- motor/move_motor.py
+++ motor/move_motor.py
@@ -5,9 +5,9 @@ def move_motor(step_pin, dir_pin, steps, direction):
     GPIO.output(dir_pin, direction)
     for _ in range(steps):
         GPIO.output(step_pin, GPIO.HIGH)
-        time.sleep(0.00025)  # 1ms 대기
+        time.sleep(0.0002)  # 1ms 대기
         GPIO.output(step_pin, GPIO.LOW)
-        time.sleep(0.00025)
+        time.sleep(0.0002)
 
 def move_Z_motor(step1_pin, dir1_pin, step2_pin, dir2_pin, steps, direction):
     GPIO.output(dir1_pin, direction)
