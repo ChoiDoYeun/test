8ee75bca3b7cf20bca68f7ff15247650a838b2ef
0209205 <156158663+0209205@users.noreply.github.com>
2024-07-04 23:54:16 +0900

    Update origin_test.py

--- origin_test.py
+++ origin_test.py
@@ -69,22 +69,22 @@ def calculate_steps_and_direction(current_pos, target_pos):
     return steps, direction
         
 # 리밋 스위치 동작 함수
-def move_origin(x_step,x_dir,step1_pin,step2_pin,dir1_pin,dir2_pin,y_step,y_dir):
+def move_origin(x_step, x_dir, step1_pin, step2_pin, dir1_pin, dir2_pin, y_step, y_dir):
    global current_x, current_y, current_z
-   X_STEP, X_DIR, Y_STEP, Y_DIR, Z_STEP_1, Z_DIR_1,Z_STEP_2,Z_DIR_2,A_STEP,A_DIR,LIMIT_X,LIMIT_Y,LIMIT_Z  = initialize()
+   X_STEP, X_DIR, Y_STEP, Y_DIR, Z_STEP_1, Z_DIR_1, Z_STEP_2, Z_DIR_2, A_STEP, A_DIR, LIMIT_X, LIMIT_Y, LIMIT_Z  = initialize()
 
    while LIMIT_Y == true:
-      move_motor(y_step,y_dir,1,GPIO.LOW)
+      move_motor(y_step, y_dir, 1, GPIO.LOW)
       current_y = origin.y
    time.sleep(0.01)
     
    while LIMIT_X == true:
-      move_motor(x_step,x_dir,1,GPIO.LOW)
+      move_motor(x_step, x_dir, 1, GPIO.LOW)
       current_x = origin.x
    time.sleep(0.01)
    
    while LIMIT_Z == true:
-      move_Z_motor(step1_pin, dir1_pin, step2_pin, dir2_pin,1,GPIO.LOW)
+      move_Z_motor(step1_pin, dir1_pin, step2_pin, dir2_pin, 1, GPIO.LOW)
       current_z = origin.z
    time.sleep(0.01)
 
@@ -115,7 +115,7 @@ try:
 
         time.sleep(0.5)
 
-        move_origin(x_step,x_dir,step1_pin,step2_pin,dir1_pin,dir2_pin,y_step,y_dir)
+        move_origin(x_step, x_dir, step1_pin, step2_pin, dir1_pin, dir2_pin, y_step, y_dir)
         
 except KeyboardInterrupt: #추후 stop버튼 푸쉬시 동작하도록 변경해야함
     print("Before finish, should move to original. plz wait")
