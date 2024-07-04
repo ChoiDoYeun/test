3b35ad5bd12525c0eecb294737b7f528766ee874
0209205 <156158663+0209205@users.noreply.github.com>
2024-07-04 23:39:14 +0900

    Update origin_test.py

--- origin_test.py
+++ origin_test.py
@@ -32,3 +32,32 @@ current_y = 0
 current_z = 0
 current_a = 0
 current_con = 0
+
+# 모터 동작 함수
+def move_motor(step_pin, dir_pin, steps, direction):
+    GPIO.output(dir_pin, direction)
+    for _ in range(steps):
+        GPIO.output(step_pin, GPIO.HIGH)
+        time.sleep(0.0002)  # 
+        GPIO.output(step_pin, GPIO.LOW)
+        time.sleep(0.0002)
+
+def move_Z_motor(step1_pin, dir1_pin, step2_pin, dir2_pin, steps, direction):
+    GPIO.output(dir1_pin, direction)
+    GPIO.output(dir2_pin, direction)
+    
+    for _ in range(steps):
+        GPIO.output(step1_pin, GPIO.HIGH)
+        GPIO.output(step2_pin, GPIO.HIGH)
+        time.sleep(0.0002)  # 0.0002
+        GPIO.output(step1_pin, GPIO.LOW)
+        GPIO.output(step2_pin, GPIO.LOW)
+        time.sleep(0.0002)
+
+def move_A_motor(step_pin, dir_pin, steps, direction):
+    GPIO.output(dir_pin, direction)
+    for _ in range(steps):
+        GPIO.output(step_pin, GPIO.HIGH)
+        time.sleep(0.01)  # 1ms 대기
+        GPIO.output(step_pin, GPIO.LOW)
+        time.sleep(0.01)
