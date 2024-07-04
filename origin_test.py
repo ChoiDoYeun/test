62a92accc507c6f84c0b5b8d54b1217e3b1d8a7e
0209205 <156158663+0209205@users.noreply.github.com>
2024-07-05 00:01:40 +0900

    Update origin_test.py

--- origin_test.py
+++ origin_test.py
@@ -61,7 +61,7 @@ def calculate_steps_and_direction(current_pos, target_pos):
     return steps, direction
         
 # 리밋 스위치 동작 함수
-def move_origin(x_step, x_dir, step1_pin, step2_pin, dir1_pin, dir2_pin, y_step, y_dir):
+def move_origin(y_step, y_dir, x_step, x_dir, step1_pin, step2_pin, dir1_pin, dir2_pin):
    global current_x, current_y, current_z
    X_STEP, X_DIR, Y_STEP, Y_DIR, Z_STEP_1, Z_DIR_1, Z_STEP_2, Z_DIR_2, A_STEP, A_DIR, LIMIT_X, LIMIT_Y, LIMIT_Z  = initialize()
 
@@ -107,7 +107,7 @@ try:
 
         time.sleep(1)
 
-        move_origin(x_step, x_dir, step1_pin, step2_pin, dir1_pin, dir2_pin, y_step, y_dir)
+        move_origin(y_step, y_dir, x_step, x_dir, step1_pin, step2_pin, dir1_pin, dir2_pin)
 
         time.sleep(1)
         
