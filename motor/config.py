5ab6db4c2ccd0431c4c03a5a7b1e21ef40b480ea
0209205 <156158663+0209205@users.noreply.github.com>
2024-07-04 23:23:55 +0900

    Update config.py

--- motor/config.py
+++ motor/config.py
@@ -23,9 +23,9 @@ def initialize():
     A_STEP = 10  # A모터 핀설정
     A_DIR = 9
     LIMIT_X = 5
-    limit_y_switch = 6
-    limit_z_switch = 16
+    LIMIT_Y = 6
+    LIMIT_Z = 16
     # GPIO 설정
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup([X_STEP, X_DIR, Y_STEP, Y_DIR, Z_STEP_1, Z_DIR_1,Z_STEP_2,Z_DIR_2,A_STEP,A_DIR,limit_x_switch,limit_y_switch,limit_z_switch], GPIO.OUT)
-    return X_STEP, X_DIR, Y_STEP, Y_DIR, Z_STEP_1, Z_DIR_1,Z_STEP_2,Z_DIR_2,A_STEP,A_DIR,limit_x_switch,limit_y_switch,limit_z_switch
+    GPIO.setup([X_STEP, X_DIR, Y_STEP, Y_DIR, Z_STEP_1, Z_DIR_1,Z_STEP_2,Z_DIR_2,A_STEP,A_DIR,LIMIT_X,LIMIT_Y,LIMIT_Z], GPIO.OUT)
+    return X_STEP, X_DIR, Y_STEP, Y_DIR, Z_STEP_1, Z_DIR_1,Z_STEP_2,Z_DIR_2,A_STEP,A_DIR,LIMIT_X,LIMIT_Y,LIMIT_Z
