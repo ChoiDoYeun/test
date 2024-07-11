f4e57bdbbbd48a49c3d88452db9c76a2c32d83f0
Choi Do Yeun <87837828+ChoiDoYeun@users.noreply.github.com>
2024-07-11 10:59:17 +0900

    Update origin_test.py

--- origin_test.py
+++ origin_test.py
@@ -82,18 +82,18 @@ def move_origin(y_step, y_dir, x_step, x_dir, step1_pin, step2_pin, dir1_pin, di
 try:
     while True:
 
-        while GPIO.input(LIMIT_Y) == GPIO.LOW:
+        while GPIO.input(LIMIT_Y) == GPIO.HIGH:
             print("move y")
         print("check y")
         time.sleep(0.01)
-        while GPIO.input(LIMIT_X) == GPIO.LOW:
+        while GPIO.input(LIMIT_X) == GPIO.HIGH:
             print("move x")
         print("check x")
         time.sleep(0.01)
         
-        while GPIO.input(LIMIT_Z) == GPIO.LOW:
+        while GPIO.input(LIMIT_Z) == GPIO.HIGH:
             print("move z")
-        print("z check")
+        print("check z")
         time.sleep(0.01)
         # target_x = 87
         # target_y = 85
