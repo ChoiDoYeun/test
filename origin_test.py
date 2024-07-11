8668f3305f440a8754218d6fbd972da540dbd234
Choi Do Yeun <87837828+ChoiDoYeun@users.noreply.github.com>
2024-07-11 10:39:51 +0900

    Update origin_test.py

--- origin_test.py
+++ origin_test.py
@@ -81,17 +81,17 @@ def move_origin(y_step, y_dir, x_step, x_dir, step1_pin, step2_pin, dir1_pin, di
 try:
     while True:
 
-        while LIMIT_Y == True:
+        while LIMIT_Y == False:
           print("move x")
         time.sleep(0.01)
         print("x check")
         
-        while LIMIT_X == True:
+        while LIMIT_X == False:
             print("move y")
         time.sleep(0.01)
         print("y check")
         
-        while LIMIT_Z == True:
+        while LIMIT_Z == False:
             print("move z")
         time.sleep(0.01)
         print("z check")
