8221c1fcd7fccc562ecc780974e5e04bc0c3726b
Choi Do Yeun <87837828+ChoiDoYeun@users.noreply.github.com>
2024-07-11 11:18:03 +0900

    Update origin_test.py

--- origin_test.py
+++ origin_test.py
@@ -81,7 +81,7 @@ def move_origin(y_step, y_dir, x_step, x_dir, step1_pin, step2_pin, dir1_pin, di
             current_z = origin.z
         check_origin = 1
         time.sleep(0.1)
-    break
+        break
     
 try:
     while True:
