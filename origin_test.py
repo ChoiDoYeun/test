0c49002b3872eb95a5c678c18a1a5022e9d27ffb
Choi Do Yeun <87837828+ChoiDoYeun@users.noreply.github.com>
2024-07-18 13:16:02 +0900

    origin_test.py 업데이트

--- origin_test.py
+++ origin_test.py
@@ -71,33 +71,27 @@ def calculate_steps_and_direction(current_pos, target_pos):
         
 def move_origin(Y_STEP,Y_DIR,X_STEP,X_DIR,Z_STEP_1,Z_DIR_1,Z_STEP_2,Z_DIR_2):
     global current_x, current_y, current_z
-    check_origin = 0
 
-    while check_origin == 0:
-        while GPIO.input(LIMIT_Y) == GPIO.HIGH:
-            move_motor(Y_STEP, Y_DIR, 1, GPIO.LOW)
-            print(Y_STEP)
-            print(Y_DIR)
-            print("y move")
-            current_y = origin_y
-        print("y done")
-        time.sleep(0.1)
-        while GPIO.input(LIMIT_X) == GPIO.HIGH:
-            
-            move_motor(X_STEP, X_DIR, 1, GPIO.LOW)
-            current_x = origin_x
-        print("x done")
-        time.sleep(0.1)
+    while GPIO.input(LIMIT_Y) == GPIO.HIGH:
+        move_motor(Y_STEP, Y_DIR, 1, GPIO.LOW)
+        print(Y_STEP)
+        print(Y_DIR)
+        print("y move")
+        current_y = origin_y
+    print("y done")
+    time.sleep(0.1)
+    while GPIO.input(LIMIT_X) == GPIO.HIGH:
+        move_motor(X_STEP, X_DIR, 1, GPIO.LOW)
+        current_x = origin_x
+    print("x done")
+    time.sleep(0.1)
         
-        while GPIO.input(LIMIT_Z) == GPIO.HIGH:
-            
-            move_Z_motor(Z_STEP_1, Z_DIR_1, Z_STEP_2, Z_DIR_2, 1, GPIO.LOW)
-            current_z = origin_z
+    while GPIO.input(LIMIT_Z) == GPIO.HIGH:
+        move_Z_motor(Z_STEP_1, Z_DIR_1, Z_STEP_2, Z_DIR_2, 1, GPIO.LOW)
+        current_z = origin_z
 
-        print("z done")
-        check_origin = 1
-        time.sleep(0.1)
-        break
+    print("z done")
+    time.sleep(0.1)
         
 try:
     while True:
