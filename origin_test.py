4f1ccc32faebbd629f0268baaf0ab597a263ae3e
0209205 <156158663+0209205@users.noreply.github.com>
2024-07-18 13:52:44 +0900

    Update origin_test.py

--- origin_test.py
+++ origin_test.py
@@ -101,7 +101,8 @@ try:
             move_motor(Y_STEP, Y_DIR, 1, GPIO.LOW)
             #print("y move")
             time.sleep(0.0002)
-            print(GPIO.input(LIMIT_Y) == GPIO.HIGH)
+            print(GPIO.input(LIMIT_Y))
+            print("   "+str(GPIO.HIGH))
             current_y = origin_y
         print("y done")
         time.sleep(0.1)
