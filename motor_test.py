390f96b91501af9716413c7d16f6ff22d38de3d8
0209205 <156158663+0209205@users.noreply.github.com>
2024-07-19 15:51:24 +0900

    Update motor_test.py

--- motor_test.py
+++ motor_test.py
@@ -64,7 +64,9 @@ def move_A_motor(step_pin, dir_pin, steps, direction):
 
 def move_Con_motor(step_pin, dir_pin, steps, direction):
     GPIO.output(dir_pin, direction)
-    for _ in range(steps):
+    # 스텝 수에 배율 적용
+    adjusted_steps = int(steps * 0.8)  # 0.8 배율 적용
+    for _ in range(adjusted_steps):
         GPIO.output(step_pin, GPIO.HIGH)
         time.sleep(0.0002)  # 1ms 대기
         GPIO.output(step_pin, GPIO.LOW)
@@ -121,7 +123,6 @@ try:
 
         # a모터 동작
         steps, direction = calculate_steps_and_direction(current_a, target_a) # Y축 dir방향, step수 계산
-        adjusted_steps = int(steps * 0.8)  # A 모터에 대해 0.8 배율 적용
         move_A_motor(A_STEP, A_DIR, steps, direction) # a모터 동작
         time.sleep(0.002)  # 대기 시간
         current_a = target_a  # 현재 a 위치 업데이트
