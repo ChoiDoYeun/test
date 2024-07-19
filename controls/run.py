631cdbd095ea2a5f49b5ead2e6809d4f1234563e
0209205 <156158663+0209205@users.noreply.github.com>
2024-07-19 14:17:57 +0900

    Update run.py

--- controls/run.py
+++ controls/run.py
@@ -8,6 +8,6 @@ import time
 def run():
     print("Run (원점이동) 시작")
     X_STEP, X_DIR, Y_STEP, Y_DIR, Z_STEP_1, Z_DIR_1, Z_STEP_2, Z_DIR_2, A_STEP, A_DIR, Con_STEP, Con_DIR, LIMIT_X, LIMIT_Y, LIMIT_Z = initialize()
-    move_origin(Y_STEP, Y_DIR, X_STEP, X_DIR, Z_STEP_1, Z_DIR_1, Z_STEP_2, Z_DIR_2)
+    move_origin(Y_STEP, Y_DIR, X_STEP, X_DIR, Z_STEP_1, Z_DIR_1, Z_STEP_2, Z_DIR_2, LIMIT_X, LIMIT_Y, LIMIT_Z)
     print("원점이동 완료")
     time.sleep(0.5)
