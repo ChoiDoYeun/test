35418b6cf2fc258fbfebdaefa58a9c90f03d78b6
Choi Do Yeun <87837828+ChoiDoYeun@users.noreply.github.com>
2024-07-19 10:24:41 +0900

    원점이동 추가

--- controls/run.py
+++ controls/run.py
@@ -1,13 +1,12 @@
 # run.py
 
+from motor.config import initialize, current_x, current_y, current_z,origin_x,origin_y,origin_z
 from motor.move_position import move_position_back
+from motor.move_origin import move_origin
 import time
 
 def run():
-    print("Run 시작")
-    # move_position_back(225,350,50)
-    # 원점으로 이동
-    # move_zero_point (x)
-    # move_zero_point (z)
-    # move_zero_point (y)
+    print("Run (원점이동) 시작")
+    move_origin(y_step, y_dir, x_step, x_dir, step1_pin, step2_pin, dir1_pin, dir2_pin)
+    print("원점이동 완료")
     time.sleep(0.5)
