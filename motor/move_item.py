1f65dc440796195eefcfc72aea0a810dab2113c9
Choi Do Yeun <87837828+ChoiDoYeun@users.noreply.github.com>
2024-07-20 16:50:41 +0900

    Con 모터 동작함수 추가

--- motor/move_item.py
+++ motor/move_item.py
@@ -1,5 +1,6 @@
 # move_item.py
 # A 모터 동작
+# Con 모터 동작
 from motor.move_motor import move_motor, move_A_motor
 from motor.config import initialize
 import RPi.GPIO as GPIO
@@ -19,3 +20,19 @@ def move_item_low():
     print("A모터 동작중")
     move_A_motor(A_STEP, A_DIR, steps, direction)
     print("A모터 동작완료")
+
+def move_con_high():
+    X_STEP, X_DIR, Y_STEP, Y_DIR, Z_STEP_1, Z_DIR_1,Z_STEP_2,Z_DIR_2,A_STEP,A_DIR,Con_STEP,Con_DIR,LIMIT_X,LIMIT_Y,LIMIT_Z  = initialize()
+    steps = 190
+    direction = GPIO.HIGH # 예시
+    print("con 동작중")
+    move_A_motor(Con_STEP, Con_DIR, steps, direction)
+    print("con 동작완료")
+
+def move_con_low():
+    X_STEP, X_DIR, Y_STEP, Y_DIR, Z_STEP_1, Z_DIR_1,Z_STEP_2,Z_DIR_2,A_STEP,A_DIR,Con_STEP,Con_DIR,LIMIT_X,LIMIT_Y,LIMIT_Z  = initialize()
+    steps = 190
+    direction = GPIO.LOW # 예시
+    print("con 동작중")
+    move_A_motor(Con_STEP, Con_DIR, steps, direction)
+    print("con 동작완료")
