486be6aa1899861ecd0b81353dfa5468adb8129c
Choi Do Yeun <87837828+ChoiDoYeun@users.noreply.github.com>
2024-07-20 16:52:14 +0900

    Con 모터 동작함수 추가 및 A모터 스텝 조정

--- motor/move_item.py
+++ motor/move_item.py
@@ -7,7 +7,7 @@ import RPi.GPIO as GPIO
 
 def move_item_high():
     X_STEP, X_DIR, Y_STEP, Y_DIR, Z_STEP_1, Z_DIR_1,Z_STEP_2,Z_DIR_2,A_STEP,A_DIR,Con_STEP,Con_DIR,LIMIT_X,LIMIT_Y,LIMIT_Z  = initialize()
-    steps = 500 # 예시
+    steps = 1 # 예시
     direction = GPIO.HIGH # 예시
     print("A모터 동작중")
     move_A_motor(A_STEP, A_DIR, steps, direction)
@@ -15,7 +15,7 @@ def move_item_high():
     
 def move_item_low():
     X_STEP, X_DIR, Y_STEP, Y_DIR, Z_STEP_1, Z_DIR_1,Z_STEP_2,Z_DIR_2,A_STEP,A_DIR,Con_STEP,Con_DIR,LIMIT_X,LIMIT_Y,LIMIT_Z  = initialize()
-    steps = 500 # 예시
+    steps = 1 # 예시
     direction = GPIO.LOW # 예시
     print("A모터 동작중")
     move_A_motor(A_STEP, A_DIR, steps, direction)
