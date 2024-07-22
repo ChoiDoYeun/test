9c7565a09bf04b698b98439fc965675e4f37f69a
0209205 <156158663+0209205@users.noreply.github.com>
2024-07-22 16:32:32 +0900

    move_con_high,low
    move_Con_motor 로 교체

--- motor/move_item.py
+++ motor/move_item.py
@@ -26,7 +26,7 @@ def move_con_high():
     steps = 190 *200
     direction = GPIO.HIGH # 예시
     print("con 동작중")
-    move_Con_motor(Con_STEP, Con_DIR, steps,direction)
+    move_Con_motor(Con_STEP, Con_DIR, steps, direction)
     print("con 동작완료")
 
 def move_con_low():
@@ -34,5 +34,5 @@ def move_con_low():
     steps = 190 * 200
     direction = GPIO.LOW # 예시
     print("con 동작중")
-    move_Con_motor(Con_STEP, Con_DIR, steps,direction)
+    move_Con_motor(Con_STEP, Con_DIR, steps, direction)
     print("con 동작완료")
