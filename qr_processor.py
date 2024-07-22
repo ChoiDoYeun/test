e622375dbb6ddc3df5e00d9fe348eeda9053f8d6
Choi Do Yeun <87837828+ChoiDoYeun@users.noreply.github.com>
2024-07-22 16:07:02 +0900

    qr_processor.py 업데이트

--- qr_processor.py
+++ qr_processor.py
@@ -52,7 +52,7 @@ def process_qr_code_output(warehouse):
             qr_data = capture_image(qr_code_image_path)
             print("qr 코드 감지완료")
             if qr_data:
-                cars = qr_data.split('\n')
+                cars = qr_data.split(',')
                 for car in cars:
                     if car:
                         location = warehouse.find_item(car)
