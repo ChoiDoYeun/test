8ee2dbe381b25470bfdc61763de12b72c6b1ee59
Choi Do Yeun <87837828+ChoiDoYeun@users.noreply.github.com>
2024-07-22 13:05:50 +0900

    in_out_mode.py 업데이트

--- motor/in_out_mode.py
+++ motor/in_out_mode.py
@@ -11,12 +11,12 @@ def input_mode(x,y,z):
     time.sleep(0.01)
     move_con_high()
     time.sleep(0.01)
-    move_item_low() # 입차대기장소로 와있는 상태에서 차량을 함으로 이동 # move_item_low 와 move_item_high의 위치는 바뀔수있음
+    move_item_high() # 입차대기장소로 와있는 상태에서 차량을 함으로 이동 # move_item_low 와 move_item_high의 위치는 바뀔수있음
     time.sleep(0.01)
     print("입차중 : 함에 이동완료")
     move_position_go(x,y,z) # 입차장소로 이동
     time.sleep(0.01)
-    move_item_high() 
+    move_item_low() 
     time.sleep(0.01)
     move_position_back(0,15,0) # 입차대기장소로 이동
     # 차를 함에서 입차장소로 이동
@@ -29,12 +29,12 @@ def output_mode(x,y,z):
     warehouse = Warehouse()
     move_position_go(x,y,z) # 저장장소로 이동
     time.sleep(0.01)
-    move_item_high() # 꺼내기
+    move_item_low() # 꺼내기
     print("출차중 : 함에 이동완료")
     time.sleep(0.01)
     move_position_back(0,15,0) # 출차장소로 이동
     time.sleep(0.01)
-    move_item_low()
+    move_item_high()
     print("출차중 : 출차완료")
     time.sleep(0.01)
     move_con_low()
