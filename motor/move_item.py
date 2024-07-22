c673a25d90701c83fe5237b076dc71fb606591c6
Choi Do Yeun <87837828+ChoiDoYeun@users.noreply.github.com>
2024-07-22 12:42:33 +0900

    move_item.py 업데이트

--- motor/move_item.py
+++ motor/move_item.py
@@ -7,32 +7,32 @@ import RPi.GPIO as GPIO
 
 def move_item_high():
     X_STEP, X_DIR, Y_STEP, Y_DIR, Z_STEP_1, Z_DIR_1,Z_STEP_2,Z_DIR_2,A_STEP,A_DIR,Con_STEP,Con_DIR,LIMIT_X,LIMIT_Y,LIMIT_Z  = initialize()
-    steps = 1 # 예시
-    direction = GPIO.HIGH # 예시
+    #steps = 1 # 예시
+    #direction = GPIO.HIGH # 예시
     print("A모터 동작중")
-    move_A_motor(A_STEP, A_DIR, steps, direction)
+    move_A_motor(A_STEP, A_DIR, 1, GPIO.HIGH)
     print("A모터 동작완료")
     
 def move_item_low():
     X_STEP, X_DIR, Y_STEP, Y_DIR, Z_STEP_1, Z_DIR_1,Z_STEP_2,Z_DIR_2,A_STEP,A_DIR,Con_STEP,Con_DIR,LIMIT_X,LIMIT_Y,LIMIT_Z  = initialize()
-    steps = 1 # 예시
-    direction = GPIO.LOW # 예시
+    #steps = 1 # 예시
+    #direction = GPIO.LOW # 예시
     print("A모터 동작중")
-    move_A_motor(A_STEP, A_DIR, steps, direction)
+    move_A_motor(A_STEP, A_DIR, 1, GPIO.LOW)
     print("A모터 동작완료")
 
 def move_con_high():
     X_STEP, X_DIR, Y_STEP, Y_DIR, Z_STEP_1, Z_DIR_1,Z_STEP_2,Z_DIR_2,A_STEP,A_DIR,Con_STEP,Con_DIR,LIMIT_X,LIMIT_Y,LIMIT_Z  = initialize()
-    steps = 191
-    direction = GPIO.HIGH # 예시
+    #steps = 191
+    #direction = GPIO.HIGH # 예시
     print("con 동작중")
-    move_motor(Con_STEP, Con_DIR, steps, direction)
+    move_motor(Con_STEP, Con_DIR, 191, GPIO.HIGH)
     print("con 동작완료")
 
 def move_con_low():
     X_STEP, X_DIR, Y_STEP, Y_DIR, Z_STEP_1, Z_DIR_1,Z_STEP_2,Z_DIR_2,A_STEP,A_DIR,Con_STEP,Con_DIR,LIMIT_X,LIMIT_Y,LIMIT_Z  = initialize()
-    steps = 191
-    direction = GPIO.LOW # 예시
+    #steps = 191
+    #direction = GPIO.LOW # 예시
     print("con 동작중")
-    move_motor(Con_STEP, Con_DIR, steps, direction)
+    move_motor(Con_STEP, Con_DIR, 191, GPIO.LOW)
     print("con 동작완료")
