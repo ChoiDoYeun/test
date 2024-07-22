8d626c434171e0541299ae82c74040dc3e226191
0209205 <156158663+0209205@users.noreply.github.com>
2024-07-22 16:30:57 +0900

    move_Con_motor 추가
    line 32

--- motor/move_motor.py
+++ motor/move_motor.py
@@ -28,3 +28,11 @@ def move_A_motor(step_pin, dir_pin, steps, direction):
         time.sleep(0.005)  # 1ms 대기
         GPIO.output(step_pin, GPIO.LOW)
         time.sleep(0.005)
+
+def move_Con_motor(step_pin, dir_pin, steps, direction):
+    GPIO.output(dir_pin, direction)
+    for _ in range(steps):
+        GPIO.output(step_pin, GPIO.HIGH)
+        time.sleep(0.0002)  # 1ms 대기
+        GPIO.output(step_pin, GPIO.LOW)
+        time.sleep(0.0002)
