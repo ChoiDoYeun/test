fe5b99c1d580606cc9fc3dc4b1c4c2a90fbad246
Choi Do Yeun <87837828+ChoiDoYeun@users.noreply.github.com>
2024-07-22 11:56:24 +0900

    in_out_mode.py 업데이트

--- motor/in_out_mode.py
+++ motor/in_out_mode.py
@@ -2,10 +2,13 @@
 from warehouse_class import Warehouse
 from motor.move_position import move_position_go,move_position_back
 from motor.move_item import move_item_high,move_item_low,move_con_high,move_con_low
+from motor.move_origin import move_origin
 import time
 
 def input_mode(x,y,z): 
     warehouse = Warehouse()
+    move_origin(Y_STEP,Y_DIR,X_STEP,X_DIR,Z_STEP1,Z_DIR1,Z_STEP2,Z_DIR2)
+    time.sleep(0.01)
     move_con_high()
     time.sleep(0.01)
     move_item_low() # 입차대기장소로 와있는 상태에서 차량을 함으로 이동 # move_item_low 와 move_item_high의 위치는 바뀔수있음
