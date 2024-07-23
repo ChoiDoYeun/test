6ac71c52cb6b8ad0b3708e39c26f8f5e2b2914a7
0209205 <156158663+0209205@users.noreply.github.com>
2024-07-23 12:35:23 +0900

    Update move_motor.py
    0.0002 -> 0.0001

--- motor/move_motor.py
+++ motor/move_motor.py
@@ -5,9 +5,9 @@ def move_motor(step_pin, dir_pin, steps, direction):
     GPIO.output(dir_pin, direction)
     for _ in range(steps):
         GPIO.output(step_pin, GPIO.HIGH)
-        time.sleep(0.0002)  # 1ms 대기
+        time.sleep(0.0001)  # 1ms 대기
         GPIO.output(step_pin, GPIO.LOW)
-        time.sleep(0.0002)
+        time.sleep(0.0001)
 
 def move_Z_motor(step1_pin, dir1_pin, step2_pin, dir2_pin, steps, direction):
     GPIO.output(dir1_pin, direction)
@@ -16,10 +16,10 @@ def move_Z_motor(step1_pin, dir1_pin, step2_pin, dir2_pin, steps, direction):
     for _ in range(steps):
         GPIO.output(step1_pin, GPIO.HIGH)
         GPIO.output(step2_pin, GPIO.HIGH)
-        time.sleep(0.0002)  # 0.0002
+        time.sleep(0.0001)  # 0.0002
         GPIO.output(step1_pin, GPIO.LOW)
         GPIO.output(step2_pin, GPIO.LOW)
-        time.sleep(0.0002)
+        time.sleep(0.0001)
 
 def move_A_motor(step_pin, dir_pin, steps, direction):
     GPIO.output(dir_pin, direction)
@@ -33,6 +33,6 @@ def move_Con_motor(step_pin, dir_pin, steps, direction):
     GPIO.output(dir_pin, direction)
     for _ in range(steps):
         GPIO.output(step_pin, GPIO.HIGH)
-        time.sleep(0.0002)  # 1ms 대기
+        time.sleep(0.0001)  # 1ms 대기
         GPIO.output(step_pin, GPIO.LOW)
-        time.sleep(0.0002)
+        time.sleep(0.0001)
