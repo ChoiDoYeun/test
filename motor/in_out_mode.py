9e33dcdd0cf8d795be882985e6ad10433da34dfe
0209205 <156158663+0209205@users.noreply.github.com>
2024-07-23 12:09:57 +0900

    Update in_out_mode.py

--- motor/in_out_mode.py
+++ motor/in_out_mode.py
@@ -16,11 +16,11 @@ def input_mode(x,y,z):
     print("입차중 : 함에 이동완료")
     move_position_go(x,y,z) # 입차장소로 이동
     time.sleep(0.01)
-    move_position_go(x,y,z-18) # 타워안으로 이동
+    move_position_go(x,y,z-20) # 타워안으로 이동
     time.sleep(0.01)
     move_item_low() 
     time.sleep(0.01)
-    move_position_go(x,y,z+18) # 타워밖으로로 이동
+    move_position_go(x,y,z+20) # 타워밖으로로 이동
     time.sleep(0.01)
     move_position_back(0,15,0) # 입차대기장소로 이동
     # 차를 함에서 입차장소로 이동
@@ -33,18 +33,18 @@ def output_mode(x,y,z):
     warehouse = Warehouse()
     move_position_go(x,y,z) # 저장장소로 이동
     time.sleep(0.01)
-    move_position_go(x,y,z-18) # 타워 안으로 이동
+    move_position_go(x,y,z-20) # 타워 안으로 이동
     time.sleep(0.01)
-    move_item_low() # 꺼내기
+    move_item_high() # 꺼내기
     print("출차중 : 함에 이동완료")
     time.sleep(0.01)
-    move_position_go(x,y,z+18) # 타워 밖으로 이동
+    move_position_go(x,y,z+20) # 타워 밖으로 이동
     time.sleep(0.01)
     move_position_back(0,15,0) # 출차장소로 이동
     time.sleep(0.01)
     move_con_high()
     time.sleep(0.01)
-    move_item_high()
+    move_item_low()
     print("출차중 : 출차완료")
     time.sleep(0.01)
     move_con_low()
