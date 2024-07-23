87ac719a15c969dcfb7b6b9eca045556970e8566
0209205 <156158663+0209205@users.noreply.github.com>
2024-07-23 14:28:32 +0900

    Update main.py
    existing_items = {'A12': '대형\njimny\nblue', 'A21': '중형\n콜로라\nwhite', 'A31': '중형\n콜로라\nblack'}

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from location_selected_callback import location_selected_callback
 def main():
     warehouse = Warehouse()
     # 미리 저장된 차량 정보
-    existing_items = {'A12': 'suv\n산타페\nblue', 'A21': '세단\n아반떼\ngray', 'A31': '세단\n그랜져\nblack'}
+    existing_items = {'A12': '대형\njimny\nblue', 'A21': '중형\n콜로라\nwhite', 'A31': '중형\n콜로라\nblack'}
 
     # # 미리 저장된 차량 정보 로딩
     warehouse.load_existing_items(existing_items)
