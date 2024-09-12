0494e8e9f625531d25d7b1eb27e983729744ee1e
0209205 <156158663+0209205@users.noreply.github.com>
2024-09-12 12:19:26 +0900

    Update warehouse_class.py

--- warehouse_class.py
+++ warehouse_class.py
@@ -20,9 +20,9 @@ class Warehouse:
         floor = int(location[1])
         room = int(location[2])
 
-        # X좌표 계산 : A&C는 86, B&D는 330, 2호실 = 1호실 + 76
+        # X좌표 계산 : A&C는 84, B&D는 330, 2호실 = 1호실 + 76
         x = 84 if building in ['A', 'C'] else 328
-        x += (room - 1) * 75
+        x += (room - 1) * 74
 
         # Y 좌표 계산 : 1층 증가할때마다 +84
         y = 2 + (floor - 1) * 84
