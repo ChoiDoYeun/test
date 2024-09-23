a00d562f35ae030937cc1149e5399e697490348f
0209205 <156158663+0209205@users.noreply.github.com>
2024-09-23 14:05:29 +0900

    Update move_item.py

--- motor/move_item.py
+++ motor/move_item.py
@@ -23,7 +23,7 @@ def move_item_low():
 
 def move_con_high():
     X_STEP, X_DIR, Y_STEP, Y_DIR, Z_STEP_1, Z_DIR_1,Z_STEP_2,Z_DIR_2,A_STEP,A_DIR,Con_STEP,Con_DIR,LIMIT_X,LIMIT_Y,LIMIT_Z  = initialize()
-    steps = 195 *200
+    steps = 194 *200
     direction = GPIO.HIGH # 예시
     print("con 동작중")
     move_Con_motor(Con_STEP, Con_DIR, steps, direction)
@@ -31,7 +31,7 @@ def move_con_high():
 
 def move_con_low():
     X_STEP, X_DIR, Y_STEP, Y_DIR, Z_STEP_1, Z_DIR_1,Z_STEP_2,Z_DIR_2,A_STEP,A_DIR,Con_STEP,Con_DIR,LIMIT_X,LIMIT_Y,LIMIT_Z  = initialize()
-    steps = 195 * 200
+    steps = 194 * 200
     direction = GPIO.LOW # 예시
     print("con 동작중")
     move_Con_motor(Con_STEP, Con_DIR, steps, direction)
