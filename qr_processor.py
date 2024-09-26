260820572741e206d1ca5cde1481410041aaa61d
0209205 <156158663+0209205@users.noreply.github.com>
2024-09-26 12:28:39 +0900

    Update qr_processor.py

--- qr_processor.py
+++ qr_processor.py
@@ -63,7 +63,7 @@ def process_qr_code_output(warehouse):
                             warehouse.remove_item(location)  # 차량 제거
                         else:
                             print(f"{car}는 창고에 존재하지 않습니다.")
-                time.sleep(5) # <- 시간은 인식하고 컨베이어벨트가 동작하는 시간
+                            
             else:
                 print("QR 코드를 읽을 수 없습니다.")
             # 0.5초마다 동작   
