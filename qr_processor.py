d34b17b8c4852d82f437a8c34458989eb053df3f
0209205 <156158663+0209205@users.noreply.github.com>
2024-09-26 14:17:45 +0900

    Update qr_processor.py

--- qr_processor.py
+++ qr_processor.py
@@ -101,7 +101,7 @@ def process_qr_code(warehouse,state):
                 else:
                     print("QR 코드를 읽을 수 없습니다.")
 
-            else if state == 'output':
+            elif state == 'output':
                 if qr_data:
                     cars = qr_data.split(',')
                     for car in cars:
@@ -118,7 +118,7 @@ def process_qr_code(warehouse,state):
                 else:
                     print("QR 코드를 읽을 수 없습니다.")
                     
-            else if == 'normal' :
+            elif == 'normal' :
                 print("대기중")
                 
         except StopThreadException:
