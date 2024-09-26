3e1b3168352c71834c926b2e3d2f599c1219e711
0209205 <156158663+0209205@users.noreply.github.com>
2024-09-26 14:16:23 +0900

    Update toggle_mode.py

--- controls/toggle_mode.py
+++ controls/toggle_mode.py
@@ -1,6 +1,6 @@
 # toggle_mode.py
 import threading
-from qr_processor import process_qr_code_input,process_qr_code_output, StopThreadException
+from qr_processor import process_qr_code_input,process_qr_code_output, StopThreadException,process_qr_code
 
 global qr_thread
 qr_thread = None
@@ -8,26 +8,29 @@ qr_thread = None
 def input_on(warehouse):
     global qr_thread
     # input용 qr코드 read를 위한 서보모터 동작 추가해야함
-    qr_thread = threading.Thread(target=process_qr_code_input, args=(warehouse,))
+#    qr_thread = threading.Thread(target=process_qr_code_input, args=(warehouse,))
+    qr_thread = threading.Thread(target=process_qr_code, args=(warehouse,input))
     qr_thread.start()
     
 def input_off():
     global qr_thread
     if qr_thread:
-        qr_thread.raise_exception(StopThreadException)  # 예외를 발생시킵니다.
-        qr_thread.join()  # 스레드가 종료될 때까지 기다립니다.
-        
+#        qr_thread.raise_exception(StopThreadException)  # 예외를 발생시킵니다.
+#        qr_thread.join()  # 스레드가 종료될 때까지 기다립니다.
+        qr_thread = threading.Thread(target=process_qr_code, args=(warehouse,normal))
 def output_on(warehouse):
     global qr_thread
     # output용 qr코드 read를 위한 서보모터 동작 추가해야함
-    qr_thread = threading.Thread(target=process_qr_code_output, args=(warehouse,))
-    qr_thread.start()
+#    qr_thread = threading.Thread(target=process_qr_code_output, args=(warehouse,))
+    qr_thread = threading.Thread(target=process_qr_code, args=(warehouse,output))
+#    qr_thread.start()
     
 def output_off():
     global qr_thread
     if qr_thread:
-        qr_thread.raise_exception(StopThreadException)  # 예외를 발생시킵니다.
-        qr_thread.join()  # 스레드가 종료될 때까지 기다립니다.
+#        qr_thread.raise_exception(StopThreadException)  # 예외를 발생시킵니다.
+#        qr_thread.join()  # 스레드가 종료될 때까지 기다립니다.
+        qr_thread = threading.Thread(target=process_qr_code, args=(warehouse,normal))
 
 # 스레드 객체에 예외를 발생시키는 메서드 추가
 def raise_exception(self, exc):
