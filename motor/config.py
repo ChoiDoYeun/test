f9da10cab349b35e2b4c987c68dd166fe218693a
0209205 <156158663+0209205@users.noreply.github.com>
2024-09-26 13:57:28 +0900

    Update config.py

--- motor/config.py
+++ motor/config.py
@@ -1,16 +1,16 @@
 # config.py
 import RPi.GPIO as GPIO
 
-STEPS_PER_MM = 200.0
+STEPS_PER_MM = 200
 
 # Global position variables
-current_x = 0.0
-current_y = 0.0
-current_z = 0.0
+current_x = 0
+current_y = 0
+current_z = 0
 
-origin_x = 0.0
-origin_y = 0.0
-origin_z = 0.0
+origin_x = 0
+origin_y = 0
+origin_z = 0
 
 
 
